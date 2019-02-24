050362ab3106e8db21253bb438f3802b43c323fb
Marita Isokallio <marita.isokallio@gmail.com>
2019-02-24 16:19:11 +0200

    rm wgsmetrics as slow

--- scripts/collect-alignment-metrics.sh
+++ scripts/collect-alignment-metrics.sh
@@ -11,30 +11,21 @@ QUALITIES=$5 # false if BaseRecalibration is applied and should use OQ tag, othe
 
 # Collect samtools stats table
 samtools stats \
-	-r ${FASTA} \
-	--remove-dups \
-	${INBAM} \
-	> ${OUT_PREFIX}_samtools.metrics
+    -r ${FASTA} \
+    --remove-dups \
+    ${INBAM} \
+    > ${OUT_PREFIX}_samtools.metrics
 
 # Picard's CollectAlignmentSummaryMetrics
 gatk CollectAlignmentSummaryMetrics \
-	-I ${INBAM} \
-	-O ${OUT_PREFIX}_alignment.metrics \
-	-R ${FASTA}
-
-# Picard's CollectWgsMetrics
-gatk CollectWgsMetrics \
-	-I ${INBAM} \
-	-O ${OUT_PREFIX}_wgs.metrics \
-	-R ${FASTA} \
-	--INCLUDE_BQ_HISTOGRAM true \
-	--READ_LENGTH ${READ_LENGTH} \
-	--USE_FAST_ALGORITHM true
+    -I ${INBAM} \
+    -O ${OUT_PREFIX}_alignment.metrics \
+    -R ${FASTA}
 
 # Picard's CollectQualityScoreDistribution
 gatk QualityScoreDistribution \
-	-I ${INBAM} \
-	-O ${OUT_PREFIX}_qscore.metrics \
-	--CHART ${OUT_PREFIX}_qscore.pdf \
-	--ALIGNED_READS_ONLY true \
-	--PF_READS_ONLY true
+    -I ${INBAM} \
+    -O ${OUT_PREFIX}_qscore.metrics \
+    --CHART ${OUT_PREFIX}_qscore.pdf \
+    --ALIGNED_READS_ONLY true \
+    --PF_READS_ONLY true
