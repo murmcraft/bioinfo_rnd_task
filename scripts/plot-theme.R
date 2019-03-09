24bc4137a8362e22e1c0f985baff2793252d9650
Marita Isokallio <marita.isokallio@gmail.com>
2019-03-09 18:40:46 +0200

    adjust plot labels

--- scripts/plot-theme.R
+++ scripts/plot-theme.R
@@ -1,16 +1,19 @@
 lightgrey <- brewer.pal(9, "Greys")[2]
 plottheme <- theme_bw() +
   theme(strip.background = element_blank(),
-        strip.text.x = element_text(face = "bold", size = 12),
+        strip.text.x = element_text(face = "bold", size = 8),
         panel.grid.major.y = element_line(colour = lightgrey),
         panel.grid.major.x = element_line(colour = lightgrey),
         panel.border = element_blank(),
-        plot.title = element_text(face = "bold", hjust = 0.5, size = 12),
+        panel.spacing = unit(0.6, "lines"),
+        plot.title = element_text(face = "bold", hjust = 0.5, size = 8),
         legend.position = "top",
+        legend.background = element_blank(),
         legend.title = element_blank(),
-        legend.text = element_text(size = 10),
+        legend.text = element_text(size = 6),
+        legend.key.size = unit(0.6, "line"),
         axis.line = element_line(colour = "black"),
-        axis.title.y = element_text(size = 13, face = "bold"),
-        axis.title.x = element_text(size = 13, face = "bold"),
-        axis.text.y = element_text(size = 12, face = "bold"),
-        axis.text.x = element_text(size = 12, face = "bold"))
+        axis.title.y = element_text(size = 8, face = "bold"),
+        axis.title.x = element_text(size = 8, face = "bold"),
+        axis.text.y = element_text(size = 7, face = "bold"),
+        axis.text.x = element_text(size = 7, face = "bold"))
